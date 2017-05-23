574680624f54922836a669aec35305723ae8faca
fudong_zhang <fudong_zhang@idea-source.net>
2017-05-23 09:16:04 +0800

    js与oc的交互互相调用

--- 奥迪康助听器/AppDelegate.m
+++ 奥迪康助听器/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "UMSocialWechatHandler.h"
 @interface AppDelegate ()
 
 @end
@@ -16,35 +16,50 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    /* 打开调试日志 */
+    [[UMSocialManager defaultManager] openLog:YES];
+    
+    /* 设置友盟appkey */
+    [[UMSocialManager defaultManager] setUmSocialAppkey:@"58f4129ef5ade47a7000014b"];
+    
+    /* 设置微信的appKey和appSecret */
+    [[UMSocialManager defaultManager] setPlaform:UMSocialPlatformType_WechatSession appKey:@"wx4dd19a66632d1af1" appSecret:@"66ea25ff2740092c8576ff512ef95427" redirectURL:@"http://mobile.umeng.com/social"];
+    
     return YES;
 }
-
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url
+{
+    BOOL result = [[UMSocialManager defaultManager] handleOpenURL:url];
+    if (!result) {
+        // 其他如支付等SDK的回调
+        //NSLog(@"%@",result);
+    }
+    return result;
+}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+   
 }
 
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+
 }
 
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+   
 }
 
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+   
 }
 
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+
 }
 
 
--- 奥迪康助听器/ViewController.m
+++ 奥迪康助听器/ViewController.m
@@ -7,23 +7,185 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
+#import "XLBallLoading.h"
+#import <UMSocialCore/UMSocialCore.h>
+#import<WebKit/WebKit.h>
+#define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width            // 屏幕宽
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height          // 屏幕高
+@interface ViewController ()<WKNavigationDelegate,WKScriptMessageHandler,WKUIDelegate>
+@property (nonatomic , strong)WKWebView *webView;
 @end
 
 @implementation ViewController
+{
+ 
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor  = [UIColor whiteColor];
+    [self makeUI];
+
+}
+//status  become  white
+- (UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
+}
+
+- (void)makeUI
+{
+    
+    
+    UIView *view = [UIView new];
+    [self.view addSubview:view];
+    view.backgroundColor = [UIColor colorWithRed:46/255.0 green:47/255.0 blue:49/255.0 alpha:1];
+    view.frame = CGRectMake(0, 0, SCREEN_WIDTH, 20);
+    
+
+    
+    //初始化一个WKWebViewConfiguration对象
+    WKWebViewConfiguration *config = [WKWebViewConfiguration new];
+    WKUserContentController *contentContentController = [[WKUserContentController alloc] init];
+    [contentContentController addScriptMessageHandler:self name:@"loginActive"];
+    config.userContentController = contentContentController;
+    
+    self.webView = [[WKWebView alloc]initWithFrame:CGRectMake(0, 20, SCREEN_WIDTH, SCREEN_HEIGHT-20) configuration:config];
+    [self.view addSubview:self.webView];
+    NSURL *url = [NSURL URLWithString:@"http://aodikang.idea-source.net"];
+    self.webView.navigationDelegate = self;
+    self.webView.UIDelegate = self;
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
+    
+}
+
+//- (void)loginActive
+//{
+//   
+//    NSDictionary *dic = @{
+//                          @"UnionId" :@"1234567890",
+//                          @"nickName" :@"奥特曼",
+//                          @"faceImg"  :@"嘻嘻嘻嘻.png",
+//                          @"gender"    :@"男"
+//                          };
+//    
+//    NSString *promptCode = [NSString stringWithFormat:@"appCallback(\"%@\")",dic];
+//    [self.webView evaluateJavaScript:promptCode completionHandler:^(id object, NSError * _Nullable error) {
+//        
+//        NSLog(@"=============%@",object);
+//    }];
+// 
+//}
+//微信登陆
+- (void)weixin
+{
+    // 在需要进行获取登录信息的UIViewController中加入如下代
+        [[UMSocialManager defaultManager] getUserInfoWithPlatform:UMSocialPlatformType_WechatSession currentViewController:self completion:^(id result, NSError *error) {
+            NSLog(@"%@",error);
+            UMSocialUserInfoResponse *resp = result;
+            // 第三方登录数据(为空表示平台未提供)
+            NSLog(@">>>>>>>>>%@",resp);
+            NSLog(@" uid: %@", resp.uid);
+            NSLog(@" openid: %@", resp.openid);
+            NSLog(@" accessToken: %@", resp.accessToken);
+            NSLog(@" refreshToken: %@", resp.refreshToken);
+            NSLog(@" expiration: %@", resp.expiration);
+            //用户数据
+            NSLog(@" name: %@", resp.name);
+            NSLog(@" iconurl: %@", resp.iconurl);
+            NSLog(@" gender: %@", resp.gender);
+            //第三方平台SDK原始数据
+            NSLog(@" originalResponse: %@", resp.originalResponse);
+            //appCallback(UnionId,nickName,faceImg,gender)
+            if (resp.uid==nil) {
+         
+                resp.uid =@"";
+                resp.name =@"";
+                resp.iconurl =@"";
+               resp.gender = @"";
+            }
+         
+            NSDictionary *data1 = @{
+                                  @"unionId" :resp.uid,
+                                  @"nickName" :resp.name,
+                                  @"faceImg"  :resp.iconurl,
+                                  @"gender"    :resp.gender
+                                  };
+            
+            
+            NSData *data = [NSJSONSerialization dataWithJSONObject:data1 options:NSJSONWritingPrettyPrinted error:nil];
+            NSString *paraStr = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+            // 将JSON字符串转成无换行无空格字符串
+            paraStr = [self becomeStr:paraStr];
+            NSString *method = [NSString stringWithFormat:@"appCallback('%@')",paraStr];
+            
+            NSLog(@"JJJJJJJJ======%@",method);
+            
+            [self.webView evaluateJavaScript:method completionHandler:^(id data, NSError * _Nullable error) {
+                
+                NSLog(@"=============%@",error);
+                
+            }];
+        }];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSString *)becomeStr:(NSString *)str
+{
+    str = [str stringByReplacingOccurrencesOfString:@" " withString: @""];
+    str = [str stringByReplacingOccurrencesOfString:@"\n" withString: @""];
+    return str;
 }
+
+#pragma mark - WKScriptMessageHandler
+- (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message
+{
+    if ([@"loginActive" isEqualToString:message.name]) {
+        
+        // 打印所传过来的参数，只支持NSNumber, NSString, NSDate, NSArray, NSDictionary,NSNull类型
+        [self weixin];
+        NSLog(@"%@", message.body);
+        
+        NSLog(@"IOS login");
+    }
+}
+//开始加载时调用
+-(void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigatio{
+    
+   //开始加载网页调用此方法
+    [self show];
+}
+
+//页面加载完成之后调用
+-(void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation{
+    //网页加载完成调用此方法
+    [self hide];
+    
+    [self.webView evaluateJavaScript:@"appIosCheck()" completionHandler:^(id appIosCheck, NSError * _Nullable error) {
+        
+        NSLog(@"嘿嘿嘿");
+    }];
+}
+
+
+-(void)show{
+    //显示BallLoading
+    [XLBallLoading showInView:self.view];
+}
+
+
+-(void)hide{
+    //隐藏BallLoading
+    [XLBallLoading hideInView:self.view];
+}
+
+
+
+- (void)dealloc
+{
+  [self.webView.configuration.userContentController removeScriptMessageHandlerForName:@"loginActive"];
+}
+
 
 
 @end
