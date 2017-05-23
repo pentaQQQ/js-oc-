20a43f56f709db373076c1c48bd412529d6c4d68
fudong_zhang <fudong_zhang@idea-source.net>
2017-05-23 09:43:04 +0800

    no message

--- 奥迪康助听器/AppDelegate.m
+++ 奥迪康助听器/AppDelegate.m
@@ -3,7 +3,7 @@
 //  奥迪康助听器
 //
 //  Created by 上海点硕 on 2017/4/13.
-//  Copyright © 2017年 cbl－　点硕. All rights reserved.
+//  Copyright © 2017年 cbl. All rights reserved.
 //
 
 #import "AppDelegate.h"
